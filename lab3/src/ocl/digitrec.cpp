881b326682cef158a4c29875453642839f2bf6de
kyleluchina <ll782@cornell.edu>
2018-04-30 23:13:35 -0400

    Debug6

--- lab3/src/ocl/digitrec.cpp
+++ lab3/src/ocl/digitrec.cpp
@@ -5,51 +5,6 @@
 
  #include "../host/typedefs.h"
 
-//extern "C" 
-//{
-
- 
-  void update_knn( digit test_inst, digit train_inst, bit6_t min_distances[K_CONST] );
-  bit4_t knn_vote( bit6_t knn_set[10][K_CONST] );
-  //----------------------------------------------------------
-  // Digitrec
-  //----------------------------------------------------------
-  // @param[in] : input - the testing instance
-  // @return : the recognized digit (0~9)
-  void DigitRec( digit* training_data, digit* testing_data, bit4_t* results) 
-  { 
-    #pragma HLS INTERFACE m_axi port=training_data offset=slave bundle=gmem
-    #pragma HLS INTERFACE m_axi port=testing_data offset=slave bundle=gmem
-    #pragma HLS INTERFACE m_axi port=results offset=slave bundle=gmem
-    #pragma HLS INTERFACE s_axilite port=training_data bundle=control
-    #pragma HLS INTERFACE s_axilite port=testing_data bundle=control
-    #pragma HLS INTERFACE s_axilite port=results bundle=control
-    #pragma HLS INTERFACE s_axilite port=return bundle=control
-
-    // This array stores K minimum distances per training set
-    bit6_t knn_set[10][K_CONST];
-
-    // Initialize the knn set
-    for ( int i = 0; i < 10; i++ )
-      for ( int k = 0; k < K_CONST; k++ )
-        // Note that the max distance is 49
-        knn_set[i][k] = 50; 
-
-    // for each of the test data
-    L180: for ( int k = 0 ; k < NUM_TEST; k++){
-      // for each training set
-      L10: for ( int i = 0; i < 10; i++ ) {
-        // for each of the trainging data
-        L1800: for ( int j = 0; j < TRAINING_SIZE; j++ ) {
-          // Update the KNN set
-          update_knn( testing_data[k], training_data[i][j], knn_set[i] );
-        }
-      } 
-      // collect the results
-      results[k] = knn_vote(knn_set);
-    }
-  }
-
 
   //-----------------------------------------------------------------------
   // update_knn function
@@ -149,5 +104,59 @@
     return min_index;
   }
 
-//}
 
+
+
+
+
+
+
+extern "C" 
+{
+
+ 
+  void update_knn( digit test_inst, digit train_inst, bit6_t min_distances[K_CONST] );
+  bit4_t knn_vote( bit6_t knn_set[10][K_CONST] );
+  //----------------------------------------------------------
+  // Digitrec
+  //----------------------------------------------------------
+  // @param[in] : input - the testing instance
+  // @return : the recognized digit (0~9)
+  void DigitRec( digit* training_data, digit* testing_data, bit4_t* results) 
+  { 
+    #pragma HLS INTERFACE m_axi port=training_data offset=slave bundle=gmem
+    #pragma HLS INTERFACE m_axi port=testing_data offset=slave bundle=gmem
+    #pragma HLS INTERFACE m_axi port=results offset=slave bundle=gmem
+    #pragma HLS INTERFACE s_axilite port=training_data bundle=control
+    #pragma HLS INTERFACE s_axilite port=testing_data bundle=control
+    #pragma HLS INTERFACE s_axilite port=results bundle=control
+    #pragma HLS INTERFACE s_axilite port=return bundle=control
+
+    // This array stores K minimum distances per training set
+    bit6_t knn_set[10][K_CONST];
+
+    // Initialize the knn set
+    for ( int i = 0; i < 10; i++ )
+      for ( int k = 0; k < K_CONST; k++ )
+        // Note that the max distance is 49
+        knn_set[i][k] = 50; 
+
+    // for each of the test data
+    L180: for ( int k = 0 ; k < NUM_TEST; k++){
+      // for each training set
+      L10: for ( int i = 0; i < 10; i++ ) {
+        // for each of the trainging data
+        L1800: for ( int j = 0; j < TRAINING_SIZE; j++ ) {
+          // Update the KNN set
+          update_knn( testing_data[k], training_data[i][j], knn_set[i] );
+        }
+      } 
+      // collect the results
+      results[k] = knn_vote(knn_set);
+    }
+  }
+
+
+
+
+}
