657864fde0bff5a69b8b4cdeae881c9a794c713b
kyleluchina <ll782@cornell.edu>
2018-05-06 23:38:28 -0400

    Bash2

--- lab2/run.sh
+++ lab2/run.sh
@@ -7,26 +7,30 @@
 # export  XCL_EMULATION_MODE=true
 # emconfigutil --platform=$AWS_PLATFORM
 
-mkdir results
+
+if [ ! -d ./results ]; then
+	mkdir results
+fi
+echo "Summary of K_CONST from 1 to 5" > ./results/estimates_summary.xtxt
 
 for index in 1 2
 do 
 	echo ""; echo ""; echo "" ; echo ""
-	echo "######################################################"
+	echo "##############################################################################################"
 	echo " Compiling project..."
-	echo "######################################################"
+	echo "##############################################################################################"
 	make clean
 	make ocl OCL_TARGET=sw_emu OCL_PLATFORM=$AWS_PLATFORM K_CONST=$index
 
 	echo ""; echo ""; echo "" ; echo ""
-	echo "######################################################"
+	echo "##############################################################################################"
 	echo " Executing DigitRec"
-	echo "######################################################"
+	echo "##############################################################################################"
 	./DigitRec_host.exe -f DigitRec.sw_emu.xclbin 
 
-	echo "######################################################"
-	echo -e "Result for K_CONST=$index"
-	echo "######################################################"
+	echo "##############################################################################################"
+	echo -e "Result for K_CONST=$index" >> ./results/estimates_summary.xtxt
+	echo "##############################################################################################"
 	cat system_estimate.xtxt >> ./results/estimates_summary.xtxt
 	echo "" >>./results/estimates_summary.xtxt 
 	echo "" >>./results/estimates_summary.xtxt 
