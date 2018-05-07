f3ada0677d91b3bf437771ef11bb058d4979d4df
kyleluchina <ll782@cornell.edu>
2018-05-06 22:05:03 -0400

    Test1

--- lab2/run.sh
+++ lab2/run.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
 
-echo "#################################################"
-echo "Setting emulation configuration..."
-echo "#################################################"
-export  XCL_EMULATION_MODE=true
-emconfigutil --platform=$AWS_PLATFORM
+# echo "#################################################"
+# echo "Setting emulation configuration..."
+# echo "#################################################"
+# export  XCL_EMULATION_MODE=true
+# emconfigutil --platform=$AWS_PLATFORM
 
 echo "#################################################"
 echo "Compiling project..."
 echo "#################################################"
 make ocl OCL_TARGET=sw_emu OCL_PLATFORM=$AWS_PLATFORM K_CONST=4
 
+echo ""
+echo ""
+echo ""
 echo "#################################################"
 echo "executing DigitRec"
 echo "#################################################"
