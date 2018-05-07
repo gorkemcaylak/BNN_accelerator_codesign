7720a44b8ed7a9b0f7831111746c6b37469308b7
kyleluchina <ll782@cornell.edu>
2018-05-06 23:45:16 -0400

    Bash2

--- lab2/run.sh
+++ lab2/run.sh
@@ -11,26 +11,30 @@
 if [ ! -d ./results ]; then
 	mkdir results
 fi
-echo "Summary of K_CONST from 1 to 5" > ./results/estimates_summary.xtxt
+echo "####################################################" > ./results/estimates_summary.xtxt
+echo " Summary of K_CONST from 1 to 5" >> ./results/estimates_summary.xtxt
+echo "####################################################" >> ./results/estimates_summary.xtxt
+echo "" >>./results/estimates_summary.xtxt
+echo "" >>./results/estimates_summary.xtxt
 
-for index in 1 2
+for index in 1 2 
 do 
 	echo ""; echo ""; echo "" ; echo ""
-	echo "##############################################################################################"
+	echo "####################################################"
 	echo " Compiling project..."
-	echo "##############################################################################################"
+	echo "####################################################"
 	make clean
 	make ocl OCL_TARGET=sw_emu OCL_PLATFORM=$AWS_PLATFORM K_CONST=$index
 
 	echo ""; echo ""; echo "" ; echo ""
-	echo "##############################################################################################"
+	echo "####################################################"
 	echo " Executing DigitRec"
-	echo "##############################################################################################"
+	echo "####################################################"
 	./DigitRec_host.exe -f DigitRec.sw_emu.xclbin 
 
-	echo "##############################################################################################"
+	echo "####################################################" >> ./results/estimates_summary.xtxt
 	echo -e "Result for K_CONST=$index" >> ./results/estimates_summary.xtxt
-	echo "##############################################################################################"
+	echo "####################################################" >> ./results/estimates_summary.xtxt
 	cat system_estimate.xtxt >> ./results/estimates_summary.xtxt
 	echo "" >>./results/estimates_summary.xtxt 
 	echo "" >>./results/estimates_summary.xtxt 
