487b3928dda91d5c0dced95494464418d42250d4
kyleluchina <ll782@cornell.edu>
2018-05-22 12:11:41 -0400

    Wrapper

--- lab2-2/run_float.sh
+++ lab2-2/run_float.sh
@@ -15,7 +15,7 @@ echo "#################################################"
 echo " Setting emulation configuration..."
 echo "#################################################"
 export  XCL_EMULATION_MODE=true
-emconfigutil --xdevice=xilinx:adm-pcie-7v3:1ddr:3.0
+emconfigutil --platform=$AWS_PLATFORM
 
 # the number of iteration, default is 10
 num_iter=20
@@ -27,7 +27,7 @@ echo "####################################################"
 echo " Compiling project with NUM_ITER=$num_iter"
 echo "####################################################"
 make clean
-make -f Makefile  ocl OCL_TARGET=sw_emu OCL_DEVICE=xilinx:adm-pcie-7v3:1ddr:3.0 NUM_ITER=$num_iter
+make -f Makefile  ocl OCL_TARGET=sw_emu OCL_PLATFORM=$AWS_PLATFORM NUM_ITER=$num_iter
 
 ### EXECUTION
 echo ""; echo ""; echo "" ; echo ""
