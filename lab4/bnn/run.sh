2dc784f98616387d907957516d302646555cd33f
gorkemcaylak <gorkem.caylak@gmail.com>
2018-08-09 12:58:29 -0400

    name change, hw verified

--- lab4/bnn/run.sh
+++ lab4/bnn/run.sh
@@ -1,6 +1,6 @@
 #===============================================================#
 #                                                               #
-#                       	run1.sh                         	#
+#                       	run.sh                          	#
 #                                                               #
 #   	A bash script to run the software emulation 			#
 #																#
@@ -22,15 +22,17 @@ emconfigutil --platform=$AWS_PLATFORM
 # create some blank-line space for easy readability
 echo ""; echo ""; echo "" ; echo ""
 echo "####################################################"
-echo " Compiling project with K_CONST=$k_value"
+echo " Compiling project
 echo "####################################################"
 make clean
 make ocl OCL_TARGET=sw_emu OCL_PLATFORM=$AWS_PLATFORM K_CONST=3
+# due to harness configuration, it expects an additional parameter (K_CONST),
+# but it does not have an effect on program
 
 ### EXECUTION
 echo ""; echo ""; echo "" ; echo ""
 echo "####################################################"
-echo " Executing DigitRec with K_CONST=$k_value"
+echo " Executing DigitRec
 echo "####################################################"
 ./BNN_host.exe -f BNN.sw_emu.xclbin 
 
